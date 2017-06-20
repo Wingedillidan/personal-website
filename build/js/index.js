d019fe4a6f351cc3e5f5af3826c57816ce4af156
Collin McLean <collinm@rainn.org>
2017-06-20 19:56:04 -0400

    NAVBAR.

--- build/js/index.js
+++ build/js/index.js
@@ -1,7 +1,26 @@
-$(window).scroll(function() {
-	var wScroll = $(this).scrollTop();
+$(function() {
+	var navLock = $('nav').offset().top;
+	var navHeight = $('nav').height();
 
-	$('.main-hero--video').css({
-		'transform' : 'translate(0, ' + wScroll / 20 + '%)'
+	$(window).scroll(function() {
+		var wScroll = $(this).scrollTop();
+
+		$('.main-hero--video').css({
+			'transform' : 'translate(0, -' + wScroll / 20 + '%)'
+		});
+
+		if (wScroll > navLock) {
+			$('nav').css({
+				'position' : 'fixed',
+				'top' : 0,
+				'left' : 0,
+				'height': '60px'
+			});
+		} else if (wScroll < navLock - 10) {
+			$('nav').css({
+				'position' : 'static',
+				'height': '75px'
+			});
+		}
 	});
 });
