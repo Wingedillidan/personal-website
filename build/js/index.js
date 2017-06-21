309bd034a86fe16b798516f0610d1b6d6dd87740
Collin McLean <collinm@rainn.org>
2017-06-20 21:47:36 -0400

    First info content block (about), lots of placeholding

--- build/js/index.js
+++ build/js/index.js
@@ -1,6 +1,7 @@
 $(function() {
 	var navLock = $('nav').offset().top;
 	var navHeight = $('nav').height();
+	var navToggle = false;
 
 	$(window).scroll(function() {
 		var wScroll = $(this).scrollTop();
@@ -9,18 +10,36 @@ $(function() {
 			'transform' : 'translate(0, -' + wScroll / 20 + '%)'
 		});
 
+		/*$('.about-me div').css({
+			'transform' : 'translate(0, -' + wScroll / 20 + '%)'
+		});*/
+
 		if (wScroll > navLock) {
-			$('nav').css({
-				'position' : 'fixed',
-				'top' : 0,
-				'left' : 0,
-				'height': '60px'
-			});
-		} else if (wScroll < navLock - 10) {
-			$('nav').css({
-				'position' : 'static',
-				'height': '75px'
-			});
+			if (navToggle == false) {
+				navToggle = true;
+				$('nav').css({
+					'position' : 'fixed',
+					'top' : 0,
+					'left' : 0,
+					'height': '60px',
+					'transition' : 'all 0.5s'
+				});
+				$('nav').next().css({
+					'margin-top' : '60px'
+				});
+			}
+		} else if (wScroll < navLock) {
+			if (navToggle == true) {
+				navToggle = false;
+				$('nav').css({
+					'position' : 'static',
+					'transition': 'none',
+					'height': '75px'
+				});
+				$('nav').next().css({
+					'margin-top' : '0',
+				});
+			}
 		}
 	});
 });
